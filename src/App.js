f03ff64c254e8606147d5bc00bc716708c5b4856
JokerV <Quocvietcoi2000@gmail.com>
2019-07-08 19:35:28 +0700

    Build template

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Header from './components/Header';
+import Products from './components/Products';
+import Message from './components/Message';
+import Cart from './components/Cart';
+import Footer from './components/Footer';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Header />
+            <main id="mainContainer">
+                <div className="container">
+                    <Products />
+                    <Message />
+                    <Cart />
+                </div>
+            </main>
+            <Footer />
+        </div>
+        );
+    }
 }
 
 export default App;
