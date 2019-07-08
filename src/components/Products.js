929fe83774fc786e849faf7893aedb4a61ed5f3b
JokerV <Quocvietcoi2000@gmail.com>
2019-07-08 20:33:36 +0700

    Using redux and display products

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
 import Product from "./Product";
+import { connect } from 'react-redux';
 
 class Products extends Component {
     render() {
+        var { products } = this.props;
         return (
             <section className="section">
                 <h1 className="section-heading">Danh Sách Sản Phẩm</h1>
                 <div className="row">
-                    <Product />
-                    <Product />
-                    <Product />
+                    { this.showProducts(products) }
                 </div >
             </section>
         );
     }
+
+    showProducts = (products) => {
+        var elmProducts = [];
+        if(products){
+            elmProducts = products.map(
+                product => {
+                    return <Product key={ product.id } product={ product }/>
+                }
+            );
+        }
+        return elmProducts;
+    }
 }
 
-export default Products;
+const mapStateToProps = state => ({
+    products: state.products
+});
+
+export default connect(mapStateToProps, null)(Products);
